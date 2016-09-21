d77821714c0dcd56875a33272ab3d3739785c122
xantares <xantares09@hotmail.com>
2016-09-21 11:42:45 +0200

    Update to 4.9 (#4)
    * Update to 4.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ mkdir -p build && cd build
 cmake \
   -DCMAKE_FIND_ROOT_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCGAL_INSTALL_LIB_DIR=lib \
   -DWITH_CGAL_ImageIO=OFF -DWITH_CGAL_Qt5=OFF \
   ..
 make install -j${CPU_COUNT}
