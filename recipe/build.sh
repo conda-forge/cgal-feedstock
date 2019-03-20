85fa45954999ba9aa39afcdd094dd529c01fdb9e
Edson Tadeu M. Manoel <tadeu@esss.co>
2019-03-20 15:06:47 -0300

    Update CGAL to 4.13
    Update SWIG bindings to latest commit
    Change to link with Python library, because of failures in macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,8 @@ cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCGAL_INSTALL_LIB_DIR=lib \
-  -DWITH_CGAL_ImageIO=OFF -DWITH_CGAL_Qt5=OFF \
+  -DWITH_CGAL_ImageIO=OFF \
+  -DWITH_CGAL_Qt5=OFF \
   ..
 make install -j${CPU_COUNT}
 
@@ -17,7 +18,7 @@ cd ../..
 # language bindings are in a separate repo without releases
 git clone https://github.com/CGAL/cgal-swig-bindings.git csb
 cd csb
-git checkout 7850024f5051eeec492aa3042d0b267c875cd5c5
+git checkout d708146e14c9ec70ea5af4c9c8934742089253f9
 
 # this test requires numpy and we do not want to build-depend on it
 rm examples/python/test_aabb2.py
@@ -26,6 +27,13 @@ rm examples/python/test_aabb2.py
 rm examples/python/test_polyline_simplification_2.py
 
 mkdir build && cd build
+
+if [ `uname` == Darwin ]; then
+  # Fixes link issues with python lib in macOS
+  # See https://github.com/conda-forge/cgal-feedstock/pull/41
+  # and https://blog.tim-smith.us/2015/09/python-extension-modules-os-x/
+	export "LDFLAGS=-bundle -undefined dynamic_lookup $LDFLAGS"
+fi
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
