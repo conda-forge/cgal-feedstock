90c65568b51e0ef7ea1b6e63902608cc6260d9e3
Edson Tadeu M. Manoel <tadeu@esss.co>
2019-03-21 13:18:10 -0300

    WIP

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,10 @@ mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
 
-cmake -LAH -G"NMake Makefiles"                ^
-  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"         ^
-  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"      ^
-  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"   ^
+cmake -LAH -G"%CMAKE_GENERATOR%" ^
+  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DWITH_CGAL_ImageIO=OFF -DWITH_CGAL_Qt5=OFF ^
   ..
 if errorlevel 1 exit 1
