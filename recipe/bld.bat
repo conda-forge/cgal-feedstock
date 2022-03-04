e22c30afeb46b1d7445d099b06770d06bd47fc6e
Tobias Fischer <info@tobiasfischer.info>
2022-03-04 10:22:49 +1000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,15 +3,23 @@ del examples\python\test_polyline_simplification_2.py
 
 mkdir build && cd build
 
-set CMAKE_CONFIG=Release
-
-cmake -LAH -G"Ninja" ^
-  -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG% ^
+cmake -G "Ninja" ^
+  -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DPYTHON_MODULE_PATH="%SP_DIR%" ^
   -DBUILD_JAVA=OFF ^
-  .. || exit /B 1
+  %SRC_DIR%
+if errorlevel 1 exit 1
 
-ninja install || exit /B 1
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+:: Test.
 ctest --output-on-failure
+if errorlevel 1 exit 1
